e58be8b7a8c5a56a3b4b0895fe330d35adaa37e0
myStudyMaterials <alenjoy@outlook.in>
2015-07-10 16:12:41 +0530

    api

---
+++ SportsStoreAPI/Controllers/ProductAPIController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using SportsStoreAPI.Models;
+using SportsStore.ViewModals;
+namespace SportsStoreAPI.Controllers
+{
+    public class ProductAPIController : ApiController
+    {
+        [HttpGet]
+        public IEnumerable<ProductDetails> GetProducts()
+        {
+            return ProductHelper.GetProducts();
+        }
+    }
+}
---
+++ SportsStoreAPI/Models/ProductHelper.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SportsStore.ViewModals;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Concrete;
+
+
+namespace SportsStoreAPI.Models
+{
+    public class ProductHelper
+    {
+        public static IEnumerable<ProductDetails> GetProducts()
+        {
+            IProducts products = new ProductRepository();
+            return products.GetProducts();
+        }
+    }
+}
